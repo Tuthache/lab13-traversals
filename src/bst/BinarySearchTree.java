bfb0b22a17bce55481070dc65b091c0401afcba0
SHU <chulie2003@gmail.com>
2022-05-06 14:56:59 -0700

    updated traversal code

--- src/bst/BinarySearchTree.java
+++ src/bst/BinarySearchTree.java
@@ -151,11 +151,14 @@ public class BinarySearchTree<T extends Comparable<T>> {
 	
 	//Traverse the tree in an postorder fashion
 	//Recurse on the children and then print the value in the current node
-	public void postOrder() {
+	public void postOrder()
+	{
 		postOrderRecurse(root); 
+		System.out.println("PostOrder test commit");
 	}
 	
-	public void postOrderRecurse(BSTNode<T> node) {
+	public void postOrderRecurse(BSTNode<T> node)
+	{
 		
 	}
 	
